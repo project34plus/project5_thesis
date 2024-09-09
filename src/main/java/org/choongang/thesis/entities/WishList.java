9d336d25ea42e1a07a0a0d49a3700b30f2a7f448
heyejiyang <xoxo99312@naver.com>
2024-09-09 14:58:55 +0900

    wishList 엔티티

---
+++ src/main/java/org/choongang/thesis/entities/WishList.java
@@ -0,0 +1,19 @@
+package org.choongang.thesis.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import lombok.Data;
+import org.choongang.global.entities.BaseEntity;
+
+@Data
+@Entity
+@IdClass(WishListId.class)
+public class WishList extends BaseEntity {
+    @Id
+    private Long tid; // 논문 아이디
+    @Id
+    @Column(length = 80)
+    private String email; // 회원 이메일
+}
