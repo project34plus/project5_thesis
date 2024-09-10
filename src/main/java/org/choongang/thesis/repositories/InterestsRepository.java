1d84650a0ba718f9685498c9b837d29f7f193b7e
inseokLee999 <syt06137@naver.com>
2024-09-10 17:57:36 +0900

    interest 받아오기 작업중

--- src/main/java/org/choongang/thesis/repositories/InterestsRepository.java
+++ src/main/java/org/choongang/thesis/repositories/InterestsRepository.java
@@ -5,5 +5,8 @@ import org.choongang.thesis.entities.InterestsId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+import java.util.List;
+
 public interface InterestsRepository extends JpaRepository<Interests, InterestsId>, QuerydslPredicateExecutor<Interests> {
+    List<Interests> findByEmail(String email);
 }
