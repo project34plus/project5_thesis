7e0b83e3a14358461be0373c242ac91c7ea9c51b
jangjangji <sjjang41@naver.com>
2024-09-10 12:51:31 +0900

    이메일로 검색까지 구현

---
+++ src/main/java/org/choongang/thesis/controllers/ThesisSearch.java
@@ -0,0 +1,14 @@
+package org.choongang.thesis.controllers;
+
+import lombok.Data;
+import org.choongang.global.CommonSearch;
+
+import java.util.List;
+
+@Data
+public class ThesisSearch extends CommonSearch {
+    private List<String> category;
+    private List<String> fields;
+    private List<String> email; //회원 이메일(로그인 ID)
+
+}
