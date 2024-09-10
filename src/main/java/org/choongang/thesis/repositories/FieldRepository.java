c8004c1fee371c7185b0ab7a0c14214577b7ff4b
heyejiyang <xoxo99312@naver.com>
2024-09-10 10:52:49 +0900

    saveService

--- src/main/java/org/choongang/thesis/repositories/FieldRepository.java
+++ src/main/java/org/choongang/thesis/repositories/FieldRepository.java
@@ -4,6 +4,6 @@ import org.choongang.thesis.entities.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface FieldRepository extends JpaRepository<Field, Long> , QuerydslPredicateExecutor<Field> {
+public interface FieldRepository extends JpaRepository<Field, String> , QuerydslPredicateExecutor<Field> {
 
 }
