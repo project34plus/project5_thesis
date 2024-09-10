c94b22be93a305c6789b51b3deda3121d20412d3
jangjangji <sjjang41@naver.com>
2024-09-10 14:15:45 +0900

    이메일로 검색까지 구현

--- src/main/java/org/choongang/thesis/repositories/FieldRepository.java
+++ src/main/java/org/choongang/thesis/repositories/FieldRepository.java
@@ -4,8 +4,6 @@ import org.choongang.thesis.entities.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-import java.util.List;
-
 public interface FieldRepository extends JpaRepository<Field, String> , QuerydslPredicateExecutor<Field> {
 
 
