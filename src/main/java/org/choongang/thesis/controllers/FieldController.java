5d9e7771371a8b1d2e94c0b18d47f9748ed9a406
inseokLee999 <syt06137@naver.com>
2024-09-11 14:42:30 +0900

    field list 받아오는 api

--- src/main/java/org/choongang/thesis/controllers/FieldController.java
+++ src/main/java/org/choongang/thesis/controllers/FieldController.java
@@ -1,10 +1,13 @@
 package org.choongang.thesis.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.rests.JSONData;
 import org.choongang.thesis.repositories.FieldRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @Tag(name="field",description = "학문 분류 코드 관련 API")
@@ -16,9 +19,14 @@ public class FieldController {
 
     private final FieldRepository fieldRepository;
 
-    @Operation
+    @Operation(summary = "필드 목록 불러오기",method = "GET")
+    @ApiResponse(responseCode = "200")
+    @GetMapping("/list")
+    @PreAuthorize("permitAll()")
     public JSONData getFieldList() {
 
         return new JSONData(fieldRepository.findAll());
     }
+
+
 }
