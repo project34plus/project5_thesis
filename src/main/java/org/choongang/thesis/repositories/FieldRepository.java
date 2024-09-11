fe2a26805bdf903e58d1a3b1452c3fe8c21600eb
hyeon12 <seojh912_@naver.com>
2024-09-12 08:37:47 +0900

    KCI 기준 학문별 분류 추가

--- src/main/java/org/choongang/thesis/repositories/FieldRepository.java
+++ src/main/java/org/choongang/thesis/repositories/FieldRepository.java
@@ -3,9 +3,10 @@ package org.choongang.thesis.repositories;
 import org.choongang.thesis.entities.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
+@Transactional
 public interface FieldRepository extends JpaRepository<Field, String> , QuerydslPredicateExecutor<Field> {
     List<Field> findByIdIn(List<String> ids);
 }
--- src/main/java/org/choongang/thesis/repositories/ThesisRepository.java
+++ src/main/java/org/choongang/thesis/repositories/ThesisRepository.java
@@ -3,6 +3,8 @@ package org.choongang.thesis.repositories;
 import org.choongang.thesis.entities.Thesis;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface ThesisRepository extends JpaRepository<Thesis, Long> , QuerydslPredicateExecutor<Thesis> {
 }
