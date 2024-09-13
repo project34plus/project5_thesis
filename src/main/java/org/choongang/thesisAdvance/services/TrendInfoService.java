56fb9124e6d81593e2a9b2943f4dbd991edebe9c
hyeon12 <seojh912_@naver.com>
2024-09-13 10:09:28 +0900

    중분류별 인기순위 조회 추가중

--- src/main/java/org/choongang/thesisAdvance/services/TrendInfoService.java
+++ src/main/java/org/choongang/thesisAdvance/services/TrendInfoService.java
@@ -4,6 +4,7 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.choongang.thesis.entities.QThesisViewDaily;
 import org.choongang.thesis.entities.QUserLog;
 import org.choongang.thesisAdvance.controllers.TrendSearch;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,7 @@ public class TrendInfoService {
 
     private final JPAQueryFactory queryFactory;
 
+    // 직업별 인기 키워드 조회
     public List<Map<String, Object>> getKeywordRankingByJob(TrendSearch search) {
         LocalDate sDate = search.getSDate();
         LocalDate eDate = search.getEDate();
@@ -54,6 +56,35 @@ public class TrendInfoService {
             }).toList();
         }
         return Collections.EMPTY_LIST;
+    }
+
+    // 대분류 -> 중분류별 인기순위
+    public List<Map<String, Object>> getFieldRanking(TrendSearch search) {
+        LocalDate sDate = search.getSDate();
+        LocalDate eDate = search.getEDate();
+        if (sDate == null) {
+            return null;
+        }
+
+        QThesisViewDaily daily = QThesisViewDaily.thesisViewDaily;
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(daily.date.goe(sDate));
+
+        if (eDate != null) {
+            builder.and(daily.date.loe(eDate));
+        }
+
+        List<String> items = queryFactory.select(daily.fields)
+                .from(daily)
+                .where(builder)
+                .fetch();
+
+        if (items == null || items.isEmpty()) {
+            return null;
+        }
+
+
+        return null;
     }
 
 }
